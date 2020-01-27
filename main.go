d7f0f7472c74a56d7af5308fcebde1031cbc2747
black knight <timur@56.digital>
2020-01-26 19:04:30 -0500

    init

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "net/http"
+  "html/template"
+  "math/rand"
+)
+
+type Date struct {
+  Response string
+}
+
+func getLeapYear () string {
+  var response string
+  // get random number between 1 - 4
+  r := rand.Intn(5 - 1) + 1
+  if r % 4 == 0 {
+    response = "Leap Year"
+  } else {
+    response = "29"
+  }
+  return response
+}
+
+func s (w http.ResponseWriter, r *http.Request) {
+  t, _ := template.ParseFiles("s.html")
+  d := Date{ Response: getLeapYear() }
+  t.Execute(w, d)
+}
+
+func main () {
+  //port := os.Getenv("port")
+  port := "3000"
+  http.HandleFunc("/", s)
+  http.ListenAndServe(":"+port, nil)
+}
+
