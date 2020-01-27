b282ca35ddab674b1f7c1ca61b65cea63042c6ba
black knight <timur@56.digital>
2020-01-27 13:56:06 -0500

    type changes and responsive

--- main.go
+++ main.go
@@ -32,6 +32,9 @@ func s (w http.ResponseWriter, r *http.Request) {
 
 func main () {
   port := os.Getenv("PORT")
+  if port == "" {
+    port = "3000"
+  }
   http.HandleFunc("/", s)
   http.ListenAndServe(":"+port, nil)
 }
