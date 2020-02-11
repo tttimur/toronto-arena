673bc6f7c3df2fb2fb7d3228d895461e19f05715
Mackenzie Davidson <mwvdavidson@gmail.com>
2020-02-11 11:59:30 -0500

    territorial acknowledgment

--- main.go
+++ main.go
@@ -1,47 +1,52 @@
 package main
 
 import (
-  "net/http"
-  "html/template"
-  "math/rand"
-  "os"
+	"html/template"
+	"math/rand"
+	"net/http"
+	"os"
 )
 
 type Date struct {
-  Response string
+	Response string
 }
 
-func getLeapYear () string {
-  var response string
-  // get random number between 0-3
-  r := rand.Intn(4)
+func getLeapYear() string {
+	var response string
+	// get random number between 0-3
+	r := rand.Intn(4)
 
-  if r % 4 == 0 {
-    response = "Leap Day"
-  } else {
-    response = "29"
-  }
-  return response
+	if r%4 == 0 {
+		response = "Leap Day"
+	} else {
+		response = "29"
+	}
+	return response
 }
 
-func s (w http.ResponseWriter, r *http.Request) {
-  t, _ := template.ParseFiles("s.html")
-  d := Date{ Response: getLeapYear() }
-  t.Execute(w, d)
+func s(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("s.html")
+	d := Date{Response: getLeapYear()}
+	t.Execute(w, d)
 }
 
-func main () {
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "3000"
-  }
-
-  mux := http.NewServeMux()
-  mux.HandleFunc("/", s)
-
-  staticFiles:= http.FileServer(http.Dir("./static"))
-  mux.Handle("/static/", http.StripPrefix("/static", staticFiles))
-
-  http.ListenAndServe(":"+port, mux)
+func ta(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("ta.html")
+	t.Execute(w, r)
 }
 
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ta", ta)
+	mux.HandleFunc("/", s)
+
+	staticFiles := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static", staticFiles))
+
+	http.ListenAndServe(":"+port, mux)
+}
